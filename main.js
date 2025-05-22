c5136c918b841986a856e0472e009e61d6a73d43
Xing Yang <108479650+Flash3241@users.noreply.github.com>
2025-05-22 15:25:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-$(document).ready(function () {
-  var flame = $("#flame");
-  var txt = $("h1");
+$(document).ready(function() {
+    $(function () {
+    var flame = $("#flame");
+    var txt = $("h1");
 
-  flame.on("click", function () {
-    flame.removeClass("burn").addClass("puff");
-    $(".smoke").each(function () {
-      $(this).addClass("puff-bubble");
+    flame.on({
+        click: function () {
+        flame.removeClass("burn").addClass("puff");
+        $(".smoke").each(function () {
+            $(this).addClass("puff-bubble");
+        });
+        $("#glow").remove();
+        txt.hide().html("Happy Birthday my beautiful Bubu").delay(750).fadeIn(300);
+        $("#candle").animate(
+            {
+            opacity: ".5"
+            },
+            100
+        );
+        }
     });
-
-    $("#glow").remove();
-    txt.hide().html("Happy Birthday my beautiful Bubu").delay(750).fadeIn(300);
-    $("#candle").animate({ opacity: ".5" }, 100);
-
-    // Show images
-    $("#birthday-images").fadeIn(800);
-
-    // Confetti burst
-    confetti({
-      particleCount: 150,
-      spread: 70,
-      origin: { y: 0.6 },
-    });
-  });
-});
+});});
